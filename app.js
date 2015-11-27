8cf57e35adfa8a8dede84d0788f513402bd3782d
LKHohn <lkhohn@gmail.com>
2015-11-27 14:53:42 -0700

    lat and lng update based on pin move

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
-// In the following example, markers appear when the user clicks on the map.
-// Each marker is labeled with a single alphabetical character.
-var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var labelIndex = 0;
-
-function initialize() {
-  var bangalore = { lat: 12.97, lng: 77.59 };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
-    center: bangalore
+    // where you wnat the map to be centered
+    center: { lat: 40.5592, lng: -105.0781  }
   });
 
-  // This event listener calls addMarker() when the map is clicked.
-  google.maps.event.addListener(map, 'click', function(event) {
-    addMarker(event.latLng, map);
-  });
+var marker = new google.maps.Marker({
+  position: { lat: 40.5592, lng: -105.0781 },
+  map: map,
+  draggable: true
+});
 
-  // Add a marker at the center of the map.
-  addMarker(bangalore, map);
-}
-
-// Adds a marker to the map.
-function addMarker(location, map) {
-  // Add the marker at the clicked location, and add the next-available label
-  // from the array of alphabetical characters.
-  var marker = new google.maps.Marker({
-    position: location,
-    label: labels[labelIndex++ % labels.length],
-    map: map
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addListener(marker,'dragend', function(){
+  // console.log(marker.getPosition().lat());
+  // console.log(marker.getPosition().lng());
+  // set the new marker location's lat and lng
+    var lat = marker.getPosition().lat();
+    var lng = marker.getPosition().lng();
+    // set those values equal to the form
+    $('#lat').val(lat);
+    $('#lng').val(lng);
+  })
