94b86014570e8206fc6906fe94c488b22e7e492d
LKHohn <lkhohn@gmail.com>
2015-11-29 14:15:49 -0700

    added message to marker

--- app.js
+++ app.js
@@ -13,13 +13,19 @@
       lng: -105.0781
     },
     map: map,
-    draggable: true
+    draggable: true,
+    title: 'this is the marker'
+  });
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: 'this is the infoWindow message'
   });
 
   google.maps.event.addListener(marker, 'dragend', function() {
     // console.log(marker.getPosition().lat());
     // console.log(marker.getPosition().lng());
     // set the new marker location's lat and lng
+
     var lat = marker.getPosition().lat();
     var lng = marker.getPosition().lng();
     // set those values equal to the form
@@ -28,7 +34,8 @@
 
     localStorage.setItem('lat', lat);
     localStorage.setItem('lng', lng);
-    console.log(localStorage.getItem('lat'));
-    console.log(localStorage.getItem('lng'));
-
   });
+
+google.maps.event.addListener(marker, 'click', function(){
+  infoWindow.open(map, marker);
+});
