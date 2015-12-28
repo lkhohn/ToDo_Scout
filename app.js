f184d4a2e74340f4a1501b1f3419ff1388019dd5
LKHohn <lkhohn@gmail.com>
2015-12-28 11:44:05 -0700

    added express

--- app.js
+++ app.js
@@ -1,163 +1,60 @@
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    // where you wnat the map to be centered. This is Fort Collins
-    center: {
-      lat: 40.5592,
-      lng: -105.0781
-    }
-  });
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-  var marker = new google.maps.Marker({
-    position: {
-      // where you wnat the marker to be. This is Fort Collins
-      lat: 40.5592,
-      lng: -105.0781
-    },
-    map: map,
-    draggable: true,
-    title: 'this is the marker'
-  });
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-// content is the information to be displayed with the pin
-  var infoWindow = new google.maps.InfoWindow({
-    content: 'this is the infoWindow message'
-  });
-// when you click on the pin, this will display the infoWindow content.
-  google.maps.event.addListener(marker, 'click', function(){
-    infoWindow.open(map, marker);
-  });
+var app = express();
 
-  google.maps.event.addListener(marker, 'dragend', function() {
-    // console.log(marker.getPosition().lat());
-    // console.log(marker.getPosition().lng());
-    // set the new marker location's lat and lng
-    var lat = marker.getPosition().lat();
-    var lng = marker.getPosition().lng();
-    // set those values equal to the form
-    $('#lat').val(lat);
-    $('#lng').val(lng);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-    localStorage.setItem('lat', lat);
-    localStorage.setItem('lng', lng);
-  });
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// from the server side group project
-// map opens that automatically determines the users location
-// pin in dropped at users current location
-// user can drag dropped pin to the general meeting area
-// based on the dragged dropped pin, map shows the closest starbucks locations
+app.use('/', routes);
+app.use('/users', users);
 
-// map opens that automatically determines the users location
-// pin in dropped at users current location
-// user can drag dropped pin to the general meeting area
-// based on the dragged dropped pin, map shows the closest starbucks locations
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-// map opens that automatically determines the users location
-// pin in dropped at users current location
-// user can drag dropped pin to the general meeting area
-// based on the dragged dropped pin, map shows the closest starbucks locations
+// error handlers
 
-var map;
-var infoWindow;
-var service;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: 40.5592,
-      lng: -105.0781
-    },
-    zoom: 12
-  });
-
-  infoWindow = new google.maps.InfoWindow();
-  service = new google.maps.places.PlacesService(map);
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found');
-      map.setCenter(pos);
-      var marker = new google.maps.Marker({
-        position: pos,
-        map: map,
-        draggable: true,
-        title: "You are here! Drag the marker to the preferred meeting area."
-      });
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-  // The idle event is a debounced event, so we can query & listen without
-  // throwing too many requests at the server.
-  map.addListener('idle', performSearch);
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-    'Error: The Geolocation service failed.' :
-    'Error: Your browser doesn\'t support geolocation.');
-}
-
-function performSearch() {
-  var request = {
-    bounds: map.getBounds(),
-    // keyword: 'best view',
-    keyword: 'starbucks coffee',
-    types: ['starbucks coffee']
-  };
-  service.radarSearch(request, callback);
-}
-
-function callback(results, status) {
-  if (status !== google.maps.places.PlacesServiceStatus.OK) {
-    console.log(status);
-    return;
-  }
-  for (var i = 0, result; result = results[i]; i++) {
-    addMarker(result);
-  }
-}
-
-function addMarker(place) {
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location,
-    icon: {
-      url: 'http://maps.gstatic.com/mapfiles/circle.png',
-      anchor: new google.maps.Point(10, 10),
-      scaledSize: new google.maps.Size(10, 17)
-    }
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    service.getDetails(place, function(result, status) {
-      if (status !== google.maps.places.PlacesServiceStatus.OK) {
-        console.error(status);
-        return;
-      }
-      // ALL buttons will open a infoWindow if hours are NOT requested
-      // infoWindow.setContent(result.name + "<br />" + result.formatted_address + "<br />" + result.website + "<br />" + result.formatted_phone_number);
-
-      // request includes hours. NOTE: NOT ALL buttons will display infoWindow
-      infoWindow.setContent(result.name + "<br />" + result.formatted_address + "<br />" + result.website + "<br />" + result.formatted_phone_number + "<br />" + result.opening_hours.weekday_text);
-      infoWindow.open(map, marker);
-
-      $('#mtgAddress').val(result.formatted_address);
-
-      var lat = marker.getPosition().lat();
-      var lng = marker.getPosition().lng();
-      // set those values equal to the form
-      $('#loc_lat').val(lat);
-      $('#loc_lng').val(lng);
-
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
   });
 }
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ public/javascripts/maps.js
@@ -0,0 +1,163 @@
+var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    // where you wnat the map to be centered. This is Fort Collins
+    center: {
+      lat: 40.5592,
+      lng: -105.0781
+    }
+  });
+
+  var marker = new google.maps.Marker({
+    position: {
+      // where you wnat the marker to be. This is Fort Collins
+      lat: 40.5592,
+      lng: -105.0781
+    },
+    map: map,
+    draggable: true,
+    title: 'this is the marker'
+  });
+
+// content is the information to be displayed with the pin
+  var infoWindow = new google.maps.InfoWindow({
+    content: 'this is the infoWindow message'
+  });
+// when you click on the pin, this will display the infoWindow content.
+  google.maps.event.addListener(marker, 'click', function(){
+    infoWindow.open(map, marker);
+  });
+
+  google.maps.event.addListener(marker, 'dragend', function() {
+    // console.log(marker.getPosition().lat());
+    // console.log(marker.getPosition().lng());
+    // set the new marker location's lat and lng
+    var lat = marker.getPosition().lat();
+    var lng = marker.getPosition().lng();
+    // set those values equal to the form
+    $('#lat').val(lat);
+    $('#lng').val(lng);
+
+    localStorage.setItem('lat', lat);
+    localStorage.setItem('lng', lng);
+  });
+
+// from the server side group project
+// map opens that automatically determines the users location
+// pin in dropped at users current location
+// user can drag dropped pin to the general meeting area
+// based on the dragged dropped pin, map shows the closest starbucks locations
+
+// map opens that automatically determines the users location
+// pin in dropped at users current location
+// user can drag dropped pin to the general meeting area
+// based on the dragged dropped pin, map shows the closest starbucks locations
+
+// map opens that automatically determines the users location
+// pin in dropped at users current location
+// user can drag dropped pin to the general meeting area
+// based on the dragged dropped pin, map shows the closest starbucks locations
+
+var map;
+var infoWindow;
+var service;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 40.5592,
+      lng: -105.0781
+    },
+    zoom: 12
+  });
+
+  infoWindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found');
+      map.setCenter(pos);
+      var marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        draggable: true,
+        title: "You are here! Drag the marker to the preferred meeting area."
+      });
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+  // The idle event is a debounced event, so we can query & listen without
+  // throwing too many requests at the server.
+  map.addListener('idle', performSearch);
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+}
+
+function performSearch() {
+  var request = {
+    bounds: map.getBounds(),
+    // keyword: 'best view',
+    keyword: 'starbucks coffee',
+    types: ['starbucks coffee']
+  };
+  service.radarSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status !== google.maps.places.PlacesServiceStatus.OK) {
+    console.log(status);
+    return;
+  }
+  for (var i = 0, result; result = results[i]; i++) {
+    addMarker(result);
+  }
+}
+
+function addMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    icon: {
+      url: 'http://maps.gstatic.com/mapfiles/circle.png',
+      anchor: new google.maps.Point(10, 10),
+      scaledSize: new google.maps.Size(10, 17)
+    }
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    service.getDetails(place, function(result, status) {
+      if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        console.error(status);
+        return;
+      }
+      // ALL buttons will open a infoWindow if hours are NOT requested
+      // infoWindow.setContent(result.name + "<br />" + result.formatted_address + "<br />" + result.website + "<br />" + result.formatted_phone_number);
+
+      // request includes hours. NOTE: NOT ALL buttons will display infoWindow
+      infoWindow.setContent(result.name + "<br />" + result.formatted_address + "<br />" + result.website + "<br />" + result.formatted_phone_number + "<br />" + result.opening_hours.weekday_text);
+      infoWindow.open(map, marker);
+
+      $('#mtgAddress').val(result.formatted_address);
+
+      var lat = marker.getPosition().lat();
+      var lng = marker.getPosition().lng();
+      // set those values equal to the form
+      $('#loc_lat').val(lat);
+      $('#loc_lng').val(lng);
+
+    });
+  });
+}
