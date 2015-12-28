92d115ba484f6711d78ba0a786833aaea1117943
LKHohn <lkhohn@gmail.com>
2015-12-28 13:17:20 -0700

    working on auth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')({
+  client:'pg',
+  connection:{
+    host:'127.0.0.1',
+    port:5432,
+    username: 'lindsayhohn',
+    database: 'todoscout_temp'
+  }
+});
+var bcrypt = require('bcrypt');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/login', function(req, res, next){
+  res.render('./login');
+});
+
+router.post('/login', function(req, res, next){
+  var username = req.body.username;
+  var password = req.body.password;
+  res.send('this username and password are: ' + username + ' ' + password);
+});
+
+
+
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+var knex = require('knex')({
+  client:'pg',
+  connection:{
+    host:'127.0.0.1',
+    port:5432,
+    username: 'lindsayhohn',
+    database: 'todoscout_temp'
+  }
+});
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+
+
+//TODO: get user/signup renders signup.jade
+//NOTE: look at index.js for examples on how to render jade layouts.
+
+//TODO: post user/signup creates a user
+//NOTE: in app.js we set up the middleware body-parser which allows us to access
+//      the post body with `req.body`
+
+//TODO: get user/signin renders signin.jade
+//NOTE: look at index.js for examples on how to render jade layouts.
+
+//TODO: post user/signin sign in a user
+//NOTE: in app.js we set up the middleware body-parser which allows us to access
+//      the post body with `req.body`
+//NOTE: in app.js we set up the middleware cookie-session which allows us to
+//      create session cookies by setting `res.session` to any value. We can
+//      use this session to store the users login info
+
+//TODO: post user/signout deletes user cookie.
+//NOTE: `req.session` should hold all the session information.
+//NOTE: in app.js we set up the middleware body-parser which allows us to access
+//      the post body with `req.body`
+
 
 module.exports = router;
